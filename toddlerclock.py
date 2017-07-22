381df2323a797e3ef59d7aba2b145b544822d50b
Tanya Reilly <tanya.reilly@gmail.com>
2017-07-22 00:27:26 -0400

    Bedside clock to tell a toddler whether they can wake you.

---
+++ toddlerclock.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""A clock to tell your toddler whether they can wake you."""
+
+import os
+import time
+import pygame
+
+
+WHITE = ((255, 255, 255))
+BLACK = ((0, 0, 0))
+RED = ((255, 0, 0))
+BLUE = ((5, 20, 114))
+GREEN = ((52, 114, 5))
+
+class ToddlerClock(object):
+  """Display the time with a message."""
+
+  def __init__(self):
+    """ """
+    # Needed to talk to the raspberry pi's PiTFT display.
+    os.putenv('SDL_FBDEV', '/dev/fb1')
+    os.putenv('SDL_MOUSEDRV', 'TSLIB')
+    os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
+
+    self.x = 320
+    self.y = 240
+    self.bottom = 200
+    pygame.init()
+    pygame.mouse.set_visible(False)
+    self.screen = pygame.display.set_mode((self.x, self.y))
+    self.clocksurface = pygame.Surface((self.x, self.bottom)).convert()
+    self.messagesurface = pygame.Surface((self.x, self.y - self.bottom)).convert()
+
+    self.bigfont = pygame.font.SysFont("verdana", 90)
+    self.smallfont = pygame.font.SysFont("verdana", 18)
+    self.clock = pygame.time.Clock()
+
+  def run(self):
+    """The main loop."""
+    while True:
+      self.screen.blit(self.clocksurface, ((0, 0)))
+      self.screen.blit(self.messagesurface, ((0, self.bottom)))
+
+      now = time.localtime()
+      nowstr = self.bigfont.render("%.2d:%.2d" % (now.tm_hour, now.tm_min),
+                                   True, WHITE)
+      if now.tm_hour < 6:
+        color = BLACK
+        message = self.smallfont.render(
+            "Too early. Go back to sleep, Biz.", True, RED) # red
+      elif now.tm_hour < 7:
+        color = BLUE
+        message = self.smallfont.render(
+            "It's early, but reading is ok.", True, WHITE)
+      else:
+        color = GREEN
+        message = self.smallfont.render(
+            "Hurray, it's morning!", True, WHITE)
+
+      self.draw(color, nowstr, message)
+      self.clock.tick(1) # every 1s
+
+  def draw(self, color, nowstr, message):
+    """Draw the clock with a message!"""
+    self.clocksurface.fill(color)
+    self.messagesurface.fill(color)
+
+    self.clocksurface.blit(nowstr,
+                           ((self.x - nowstr.get_width()) / 2,
+                            (self.bottom - nowstr.get_height()) / 2))
+    self.messagesurface.blit(message,
+                             ((self.x - message.get_width()) / 2,
+                              (self.y - self.bottom - message.get_height()) / 2))
+    pygame.display.flip()
+
+
+# Main.
+toddlerclock = ToddlerClock()
+toddlerclock.run()
