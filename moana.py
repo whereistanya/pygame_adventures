3ad743adcc9ffd0ab68f1bd10f811a7a43e258b5
Tanya Reilly <tanya.reilly@gmail.com>
2018-07-06 20:18:27 -0400

    Add a boat for moana to find before they can pick up shells.

--- moana.py
+++ moana.py
@@ -12,10 +12,12 @@ class AmazingMoanaGame(object):
 
   def __init__(self, square_size=128, max_x=15, max_y=7,
                moana_image="images/babymoana.jpg",
+               moana_boat_image="images/moana_boat.jpg",
                maui_image="images/maui.jpg",
                sharkhead_image="images/maui_sharkhead.jpg",
                crab_image="images/crab.jpg",
                hook_image="images/fishhook.jpg",
+               boat_image="images/boat.jpg",
                shells_image="images/shell.png",
                mud_image="images/mud.png"):
     """Set up the game.
@@ -42,11 +44,14 @@ class AmazingMoanaGame(object):
     self.done = False
     self.drawer = drawer.Drawer(square_size, max_x, max_y)
     self.moana = things.MovingThing(self.get_image(moana_image), self.drawer)
+    self.moana.add_replacement_image(self.get_image(moana_boat_image))
     self.maui = things.MovingThing(self.get_image(sharkhead_image), self.drawer, x=max_x - 1)
     self.maui.add_replacement_image(self.get_image(maui_image))
     self.crab = things.SelfMovingThing(self.get_image(crab_image), self.drawer, x=int(max_x / 2))
     self.hook = things.StationaryThings(self.get_image(hook_image), self.drawer)
     self.hook.place_randomly(1)
+    self.boat = things.StationaryThings(self.get_image(boat_image), self.drawer)
+    self.boat.place_randomly(1)
     self.mud = things.StationaryThings(self.get_image(mud_image), self.drawer, obstacle=True)
     self.mud.place_randomly(15)
     self.shells = things.StationaryThings(self.get_image(shells_image), self.drawer)
@@ -54,6 +59,7 @@ class AmazingMoanaGame(object):
     self.drawer.set_background((0, 0, 255))  # blue
     self.drawer.update_score_text("Get the hook!")
     self.has_hook = False
+    self.has_boat = False
 
   def run(self):
     """The main game loop. Draw stuff and look for events."""
@@ -64,6 +70,7 @@ class AmazingMoanaGame(object):
       self.shells.draw()
       self.crab.move_up_and_down()
       self.hook.draw()
+      self.boat.draw()
       self.moana.draw()
       self.maui.draw()
       self.crab.draw()
@@ -76,6 +83,7 @@ class AmazingMoanaGame(object):
           self.maui.set_default_image()
           self.drawer.update_score_text("You LOST the hook!")
 
+      if self.has_hook and self.has_boat:
         if self.shells.is_at(self.moana.pos()):
           self.shells.delete(self.moana.pos())
           self.moana.score += 1
@@ -92,6 +100,21 @@ class AmazingMoanaGame(object):
         self.maui.set_replacement_image()
         self.has_hook = True
         self.drawer.update_score_text("You got the hook!")
+        if self.has_boat:
+          # we're ready to begin!
+          self.drawer.set_background((255, 64, 0))  # orange
+
+      # Only Moana can get the boat.
+      if self.boat.is_at(self.moana.pos()):
+        self.boat.delete(self.moana.pos())
+        self.drawer.set_background((255, 102, 255))  # pink
+        self.moana.set_replacement_image()
+        self.has_boat = True
+        self.drawer.update_score_text("You got the boat!")
+        if self.has_hook:
+          # we're ready to begin!
+          self.drawer.set_background((255, 64, 0))  # orange
+
 
       self.drawer.show_messages()
       pygame.display.flip()
